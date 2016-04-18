8752ab5474b059c953173ac409ddf04de35a8fbc
TravisHoover <traviskhoover@gmail.com>
2016-04-18 11:00:54 -0500

    Update mm.c

--- labMalloc/mm.c
+++ labMalloc/mm.c
@@ -26,12 +26,12 @@
 #define PREV_BLKP(bp)       ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
 #define PUT_HDR_FTR(bp, size, alloc) PUT(HDRP(bp), PACK(size, alloc)); PUT(FTRP(bp), PACK(size, alloc));
 
-static void *heapExtension();
-static inline void *coalesce();
+static void *heapExtension(size_t);
+static void *coalesce(void *);
 static void *find_fit(size_t size);
 static void *find_fit_from_to(size_t size, void *from, void *to);
-static inline void place(void *bp, size_t size);
-static char * heap_listp;
+static void place(void *bp, size_t size);
+static char * heap_listp; //pointer to first block
 
 /*
  * mm_init - initialize the malloc package.
@@ -53,18 +53,21 @@ int mm_init(void)
     return 0;
 }
 
+
+
 static void *heapExtension(size_t words)
 {
     char *bp;
     size_t size;
 
-    // Allocate evenly
+    // Allocate even number of words to maintain alignment
     size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
     if ((long)(bp = mem_sbrk(size)) == -1)
         return NULL;
 
 
     // Initialize free block header/footer and the epilogue header
+    // PUT_HDR_FTR(bp, size, 0);
     PUT_HDR_FTR(bp, size, 0);
     PUT(HDRP(NEXT_BLKP(bp)), PACK(0,1));
 
@@ -81,6 +84,7 @@ void *mm_malloc(size_t size)
     size_t extend_size; // amount to extend if no fit
     char *bp;
 
+    // ignore pointless calls
     if ( size == 0 ) return NULL;
 
     if ( size <= DSIZE )
@@ -102,60 +106,13 @@ void *mm_malloc(size_t size)
     return bp;
 }
 
-static char *last_find = NULL;
-static void *find_fit(size_t size)
-{
-    if ( last_find == NULL )
-    {
-        last_find = find_fit_from_to(size, NEXT_BLKP(heap_listp), mem_heap_hi());
-        return last_find;
-    }
-
-
-    if ( (last_find = find_fit_from_to(size, NEXT_BLKP(last_find), mem_heap_hi())) == NULL )
-        // didn't find anything from last find to end. run from beginning to last find
-        last_find = find_fit_from_to(size, NEXT_BLKP(heap_listp), last_find);
-
-    return last_find;
-}
-
-static void *find_fit_from_to(size_t size, void *from, void *to)
-{
-    void *bp = from;
-    while(!(GET_SIZE(HDRP(bp)) >= size && GET_ALLOC(HDRP(bp)) == 0))
-    {
-        if ( bp >= to )
-            return NULL;
-
-        bp = NEXT_BLKP(bp);
-    }
-
-    return bp;
-}
-
-inline static void place(void *bp, size_t size)
-{
-    size_t curr_size = GET_SIZE(HDRP(bp)); // current size
-
-    // If there is enough room for another block, split.
-    if ((curr_size - size) >= 2*DSIZE)
-    {
-        PUT_HDR_FTR(bp, size, 1);
-        PUT_HDR_FTR(NEXT_BLKP(bp), (curr_size - size), 0);
-    }
-    else
-    {
-        PUT_HDR_FTR(bp, curr_size, 1);
-    }
-}
-
 /*
  * mm_free - Freeing a block does nothing.
  */
 void mm_free(void *bp)
 {
     size_t size;
-    // slight optimization.
+    // slight optimization. If it's already freed, skip the coalescing
     if ( GET_ALLOC(HDRP(bp)) == 0 )
         return;
 
@@ -163,59 +120,6 @@ void mm_free(void *bp)
 
     PUT_HDR_FTR(bp, size, 0);
     coalesce(bp);
-}
-
-
-inline static void *coalesce(void *bp)
-{
-    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
-    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
-    size_t size = GET_SIZE(HDRP(bp));
-
-    // both previous and next blocks are allocated; nothing to do
-    if (prev_alloc && next_alloc)
-    {
-        return bp;
-    }
-        // previous is allocated but next is free
-    else if (prev_alloc && !next_alloc)
-    {
-        // if the last_find pointer was at the coalesced block, point
-        // to the block after next
-        if ( last_find == NEXT_BLKP(bp) )
-            last_find = NEXT_BLKP(NEXT_BLKP(bp));
-
-        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
-
-        PUT_HDR_FTR(bp, size, 0);
-    }
-        // previous is free but next is allocated
-    else if (!prev_alloc && next_alloc)
-    {
-        // if the last_find pointer is pointing to bp, set it to point
-        // to the next block
-        if ( last_find == bp )
-            last_find = NEXT_BLKP(bp);
-
-        size += GET_SIZE(FTRP(PREV_BLKP(bp)));
-        PUT(FTRP(bp), PACK(size, 0));
-        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
-        bp = PREV_BLKP(bp);
-    }
-        // both are free
-    else
-    {
-        // if the last_find pointer is in coalesced block, point
-        // to the block after next
-        if ( last_find == NEXT_BLKP(bp) || last_find == bp )
-            last_find = NEXT_BLKP(NEXT_BLKP(bp));
-
-        size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
-        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
-        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
-        bp = PREV_BLKP(bp);
-    }
-    return bp;
 }
 
 /*
@@ -266,4 +170,95 @@ void *mm_realloc(void *bp, size_t size)
     place(new_bp, adj_size);
 
     return new_bp;
+}
+
+static char *last_find = NULL;
+static void *find_fit(size_t size)
+{
+    if ( last_find == NULL )
+    {
+        // find fit hasn't run yet. run from beginning to end of heap
+        last_find = find_fit_from_to(size, NEXT_BLKP(heap_listp), mem_heap_hi());
+        return last_find;
+    }
+
+    // find fit from last find to end
+
+    if ( (last_find = find_fit_from_to(size, NEXT_BLKP(last_find), mem_heap_hi())) == NULL )
+        // didn't find anything from last find to end. run from beginning to last find
+        last_find = find_fit_from_to(size, NEXT_BLKP(heap_listp), last_find);
+
+    return last_find;
+}
+
+static void *find_fit_from_to(size_t size, void *from, void *to)
+{
+    void *bp = from;
+    while(!(GET_SIZE(HDRP(bp)) >= size && GET_ALLOC(HDRP(bp)) == 0))
+    {
+        if ( bp >= to )
+            return NULL;
+
+        bp = NEXT_BLKP(bp);
+    }
+
+    return bp;
+}
+ static void place(void *bp, size_t size)
+{
+    size_t curr_size = GET_SIZE(HDRP(bp)); // current size
+
+    if ((curr_size - size) >= 2*DSIZE)
+    {
+        PUT_HDR_FTR(bp, size, 1);
+        PUT_HDR_FTR(NEXT_BLKP(bp), (curr_size - size), 0);
+    }
+    else
+    {
+        PUT_HDR_FTR(bp, curr_size, 1);
+    }
+}
+
+
+
+ static void *coalesce(void *bp)
+{
+    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+    size_t size = GET_SIZE(HDRP(bp));
+
+    if (prev_alloc && next_alloc) //do nothing if both are allocated already
+    {
+        return bp;
+    }
+    else if (prev_alloc)
+    {
+        if ( last_find == NEXT_BLKP(bp) ) //skip block if last find point is pointing to coalesced block
+            last_find = NEXT_BLKP(NEXT_BLKP(bp));
+
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+
+        PUT_HDR_FTR(bp, size, 0);
+    }
+    else if (next_alloc)
+    {
+        if ( last_find == bp )
+            last_find = NEXT_BLKP(bp);
+
+        size += GET_SIZE(FTRP(PREV_BLKP(bp)));
+        PUT(FTRP(bp), PACK(size, 0));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }
+    else
+    {
+        if ( last_find == NEXT_BLKP(bp) || last_find == bp ) //skip a block if next block is coalesced
+            last_find = NEXT_BLKP(NEXT_BLKP(bp));
+
+        size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }
+    return bp;
 }
